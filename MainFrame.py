8c24bea6ed4398a13dcb7fb148e15e2016de7663
Filbert-code <alexlingfungfilbert@gmail.com>
2021-11-23 17:40:54 -0600

    changed the layout to using pack instead of grid

--- MainFrame.py
+++ MainFrame.py
@@ -12,23 +12,23 @@ class MainFrame(tk.Frame):
         # configurations for the outer-most frame
         self.parent = parent
         self.parent.title("Metro-2 Text Replacement Tool")
-        self.parent.maxsize(800, 600)
-        self.parent.minsize(800, 600)
+        # self.parent.maxsize(800, 600)
+        # self.parent.minsize(800, 600)
         self.config(bg='lightseagreen', width=800, height=600)
         self.grid(row=0, column=0)
-        self.grid_propagate(0)
+        # self.grid_propagate(0)
 
         self.text = open("1000MostCommonWords.txt").readlines()
 
         self.movingText = MovingText(self)
-        self.movingText.grid(row=0, column=0)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.movingText.pack(anchor=tk.CENTER, padx=20, pady=40)
+        # self.grid_rowconfigure(0, weight=1)
+        # self.grid_columnconfigure(0, weight=1)
 
         self.entry = UserEntry(self)
-        self.entry.grid(row=1, column=0)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        self.entry.pack(anchor=tk.CENTER)
+        # self.grid_rowconfigure(1, weight=1)
+        # self.grid_columnconfigure(1, weight=1)
 
 
 
